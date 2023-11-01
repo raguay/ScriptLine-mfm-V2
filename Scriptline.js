2bbe6f8af7b80c11c48cb941191fc15fb0bb9393
Richard Guay <raguay@customct.com>
2023-11-01 14:23:44 +0700

    Updating the package file

--- Scriptline.js
+++ Scriptline.js
@@ -16,7 +16,7 @@ const ScriptLine = {
     console.log("ScriptLine: ", dir);
     await ScriptLine.fs.runCommandLine(`wget -O /dev/null -q -nv --method=PUT --body-data="{\"wd\": \"${dir}\"}" --header='Content-Type:application/json' 'http://localhost:9978/api/wd' 2>&1 > /dev/null`, [], (err, _) => {
       if (err) {
-        console.log("ScriptLine: ", err.Error())
+        console.log("ScriptLine: ", err);
       }
     }, null);
   }
