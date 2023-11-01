3ec8227f06a72be8f7fff18340f8ee9e3d8bfa13
Richard Guay <raguay@customct.com>
2023-11-01 15:54:36 +0700

    Fixing not sending to EmailIt

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This exention will update the ScriptLine feature of [EmailIt](https://GitHub.com/raguay/EmailIt) with the current directory each time there is a directory change.
 
+In order to use this extension, you must have wget command in your path.
--- Scriptline.js
+++ Scriptline.js
@@ -13,11 +13,9 @@ const ScriptLine = {
   installKeyMaps: function() {
   },
   DirListener: async function(dir) {
-    console.log("ScriptLine: ", dir);
-    await ScriptLine.fs.runCommandLine(`wget -O /dev/null -q -nv --method=PUT --body-data="{\"wd\": \"${dir}\"}" --header='Content-Type:application/json' 'http://localhost:9978/api/wd' 2>&1 > /dev/null`, [], (err, stdout) => {
-      console.log(stdout);
+    await ScriptLine.fs.runCommandLine(`wget -O /dev/null -q -nv --method=PUT --body-data='{"wd": "${dir}"}' --header="Content-Type:application/json" "http://localhost:9978/api/wd"`, [], (err, _) => {
       if (err) {
-        console.log("ScriptLine: ", err);
+        console.log("ScriptLine Error: ", err);
       }
     }, null);
   }
