a163a4891c69715e40937d1f32c72cef96d18155
Richard Guay <raguay@customct.com>
2023-11-01 14:20:16 +0700

    Updating the package file

--- Scriptline.js
+++ Scriptline.js
@@ -13,6 +13,7 @@ const ScriptLine = {
   installKeyMaps: function() {
   },
   DirListener: async function(dir) {
+    console.log("ScriptLine: ", dir);
     await ScriptLine.fs.runCommandLine(`wget -O /dev/null -q -nv --method=PUT --body-data="{\"wd\": \"${dir}\"}" --header='Content-Type:application/json' 'http://localhost:9978/api/wd' 2>&1 > /dev/null`, [], (err, _) => {
       if (err) {
         console.log("ScriptLine: ", err.Error())
